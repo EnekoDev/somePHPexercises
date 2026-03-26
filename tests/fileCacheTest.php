74388ae1a463350f6e85e348774881ca16bf213f
EnekoDev <enekolabdev@gmail.com>
2026-03-26 16:58:27 +0100

    testing fileCache

--- tests/fileCacheTest.php
+++ tests/fileCacheTest.php
@@ -5,12 +5,21 @@ use PHPUNIT\Framework\TestCase;
 require_once __DIR__ . "/../src/level1/fileCache.php";
 
 class FileCacheTest extends TestCase {
+    private string $cacheDir = __DIR__ . "/../src/level1/cache";
+    private string $fileExt = ".cache.php";
+
     public function testCacheDirCreated() {
-        $cacheDir = __DIR__ . "/../src/level1/cache";
-        if (is_dir($cacheDir)) {
-            rmdir($cacheDir);
+        if (is_dir($this->cacheDir)) {
+            rmdir($this->cacheDir);
         }
-        new FileCache(__DIR__ . "/../src/level1/cache");
-        $this->assertDirectoryExists($cacheDir);
+        new FileCache($this->cacheDir);
+        $this->assertDirectoryExists($this->cacheDir);
+    }
+
+    public function testSetFile() {
+        $cache = new FileCache($this->cacheDir);
+        $data = ["testData" => "this data is for testing", "moreData" => "this is more data"];
+        $cache->set("test", $data);
+        $this->assertFileExists($this->cacheDir . "/test" . $this->fileExt);
     }
 }
